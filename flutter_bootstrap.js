3c5309bfd3919dc7dfff4d4ef8eaeb28072b3c2f
apple <shinji.oosaki@gmail.com>
2024-11-16 15:16:19 +0900

    test2

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "833782404"
+    serviceWorkerVersion: "642766490"
   }
 });
